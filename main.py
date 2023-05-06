0db0b9f9908a04a3951f8b827ac52a77fb0cefe6
sydneywu <sydneywhz@gmail.com>
2023-05-07 02:17:44 +0800

    update port to be number instead of string

--- main.py
+++ main.py
@@ -92,4 +92,4 @@ async def send_qa(request: SendMessageInput):
 if __name__ == "__main__":
     import uvicorn
     port = os.getenv('PORT')
-    uvicorn.run(app, host="0.0.0.0", port=port or "8000")
+    uvicorn.run(app, host="0.0.0.0", port=port or 8000)
